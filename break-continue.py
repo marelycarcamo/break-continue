e2dd0d6742b696c2be2eaf1bbac80ad76936f77f
marely <marely.carcamo@gmail.com>
2024-01-26 05:44:50 -0300

    agregado archivo readme en local

--- break-continue.py
+++ break-continue.py
@@ -1,3 +1,6 @@
+# PROGRAMA BREAK-CONTINUE
+# Este programa tiene como función ejemplificar la diferencia entre el uso de la instrucción break y continue.
+
 # break - ejemplo
 
 print("La instrucción break:")
@@ -16,4 +19,16 @@ for i in range(1, 6):
         continue
     print("Dentro del bucle.", i)
 print("Fuera del bucle.")
+
+
+# El feo devorador de vocales
+
+user_word = input("Ingrese una palabra: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A" or letter == "E" or letter == "I" or letter == "O" or letter == "U":
+        continue
+    else:
+        print(letter)
 
